17bc87cc897a7ce1a33f6ae09c7338d1eff5c5fb
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2021-06-19 22:31:16 -0400

    Algo

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -6,16 +6,16 @@ const BookItem = (props) => {
     return(
         <div>
             <div className="books-container--portada">
-                <Link href="">
+                <Link to={`/libro/${props.character.name}`}>
                     <img src={props.character.image} alt="" />
                 </Link>
                 <h3>{props.character.name}</h3>
             </div>
             <div className="autor">
-                <Link href="">{props.character.type} </Link>
+                <Link to={`/libro/${props.character.id}`}>{props.character.type} </Link>
             </div>
             <div className="descargar">
-                <Link href="">{props.character.species} {props.character.origin.name} ({props.character.status})</Link>
+                <Link to={`/libro/${props.character.name}`}>{props.character.species} {props.character.origin.name} ({props.character.status})</Link>
             </div>
         </div>
     )
